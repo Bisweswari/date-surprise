6d8f5d5a54489d44b3525e4c434cb4b00cc0effd
Bisweswari Rath <barish832000@gmail.com>
2026-06-29 16:36:19 +0530

    email.js

--- src/services/email.js
+++ src/services/email.js
@@ -1,15 +1,14 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID = "service_1mxmk6g";
-const TEMPLATE_ID = "template_9hqr9ij";
-const PUBLIC_KEY = "FQ2dTdgRr0j5EKcFA";
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 export async function sendResponse(answers) {
   const now = new Date();
 
   const templateParams = {
     name: "❤️ She Said YES ❤️",
-
     accepted: "YES ❤️",
 
     q1: answers[0] || "",
@@ -20,24 +19,18 @@ export async function sendResponse(answers) {
     q6: answers[5] || "",
 
     date: now.toLocaleDateString(),
-
     time: now.toLocaleTimeString(),
 
     message: `
 Question 1: ${answers[0] || ""}
-
 Question 2: ${answers[1] || ""}
-
 Question 3: ${answers[2] || ""}
-
 Question 4: ${answers[3] || ""}
-
 Question 5: ${answers[4] || ""}
-
 Question 6: ${answers[5] || ""}
 
 Accepted: YES ❤️
-`
+    `,
   };
 
   return emailjs.send(
